50e5a1975f1447521c7a01d6de46e5761765082d
starry <csywasde@outlook.com>
2026-02-11 20:14:59 +0800

    Update restore.sh

--- VPS/restore.sh
+++ VPS/restore.sh
@@ -13,12 +13,11 @@ while true; do
     echo -e "${GREEN}         系统快照 & 备份管理菜单        ${NC}"
     echo -e "${GREEN}========================================${NC}"
     echo -e "${GREEN}1) SSH密钥自动配置${NC}"
-    echo -e "${GREEN}2) Rclone备份${NC}"
-    echo -e "${GREEN}3) 安装快照备份${NC}"
-    echo -e "${GREEN}4) 本地系统快照恢复${NC}"
-    echo -e "${GREEN}5) 远程系统快照恢复${NC}"
-    echo -e "${GREEN}6) 卸载快照备份${NC}"
-    echo -e "${GREEN}0) 退出菜单${NC}"
+    echo -e "${GREEN}2) 安装快照备份${NC}"
+    echo -e "${GREEN}3) 本地系统快照恢复${NC}"
+    echo -e "${GREEN}4) 远程系统快照恢复${NC}"
+    echo -e "${GREEN}5) 卸载快照备份${NC}"
+    echo -e "${GREEN}0) 退出{NC}"
     read -p "$(echo -e ${GREEN}请选择操作: ${NC})" choice
 
     case $choice in
@@ -28,26 +27,21 @@ while true; do
             read -p "$(echo -e ${GREEN}按回车继续...${NC})"
             ;;
         2)
-            echo -e "${GREEN}执行 Rclone备份...${NC}"
-            bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/rclone.sh)
-            read -p "$(echo -e ${GREEN}按回车继续...${NC})"
-            ;;
-        3)
             echo -e "${GREEN}安装快照备份...${NC}"
             bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/system_snapshot.sh)
             read -p "$(echo -e ${GREEN}按回车继续...${NC})"
             ;;
-        4)
+        3)
             echo -e "${GREEN}本地系统快照恢复...${NC}"
             bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/local_restore.sh)
             read -p "$(echo -e ${GREEN}按回车继续...${NC})"
             ;;
-        5)
+        4)
             echo -e "${GREEN}远程系统快照恢复...${NC}"
             bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/remote.sh)
             read -p "$(echo -e ${GREEN}按回车继续...${NC})"
             ;;
-        6)
+        5)
             echo -e "${GREEN}卸载快照备份...${NC}"
             bash <(curl -fsSL https://raw.githubusercontent.com/sistarry/toolbox/main/VPS/uninstall_snapshot.sh)
             read -p "$(echo -e ${GREEN}按回车继续...${NC})"
