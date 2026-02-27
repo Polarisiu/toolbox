3c89b8583712219d3e7c0c3c4c22f487c11b33bd
starry <csywasde@outlook.com>
2026-02-27 20:50:24 +0800

    Update gost.sh

--- PROXY/gost.sh
+++ PROXY/gost.sh
@@ -7,30 +7,17 @@ RESET="\033[0m"
 menu() {
     clear
     echo -e "${GREEN}=== GOST 安装菜单 ===${RESET}"
-    echo -e "${GREEN}1) 国外 EZGost 安装${RESET}"
-    echo -e "${GREEN}2) 国内 EZGost 安装${RESET}"
-    echo -e "${GREEN}3) GOST 简化版安装${RESET}"
-    echo -e "${GREEN}4) GOST Panel${RESET}"
+    echo -e "${GREEN}1) EZGost安装${RESET}"
+    echo -e "${GREEN}2) GOSTPanel${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
         1)
-            echo -e "${GREEN}正在安装国外 EZGost...${RESET}"
+            echo -e "${GREEN}正在安装 EZGost...${RESET}"
             wget --no-check-certificate -O gost.sh https://raw.githubusercontent.com/qqrrooty/EZgost/main/gost.sh && chmod +x gost.sh && ./gost.sh
             pause
             ;;
         2)
-            echo -e "${GREEN}正在安装国内 EZGost...${RESET}"
-            wget --no-check-certificate -O gost.sh https://mirror.ghproxy.com/https://raw.githubusercontent.com/qqrrooty/EZgost/main/CN/gost.sh && chmod +x gost.sh && ./gost.sh
-            pause
-            ;;
-        
-        3)
-            echo -e "${GREEN}正在安装 GOST 简化版...${RESET}"
-            bash <(curl -sSL https://raw.githubusercontent.com/xmg0828-01/gost/main/gost.sh)
-            pause
-            ;;
-        4)
             echo -e "${GREEN}正在安装 GOST Panel...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/GOSTPaneldocker.sh)
             pause
