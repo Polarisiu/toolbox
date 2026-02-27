0456694365eaf4c7dd285c32df93fc6cb63e0adf
starry <csywasde@outlook.com>
2026-02-27 21:00:17 +0800

    Update realmdog.sh

--- PROXY/realmdog.sh
+++ PROXY/realmdog.sh
@@ -7,13 +7,10 @@ RESET="\033[0m"
 menu() {
     clear
     echo -e "${GREEN}=== Realm 转发管理 ===${RESET}"
-    echo -e "${GREEN}1) 国外安装 Realm 转发${RESET}"
-    echo -e "${GREEN}2) 国内安装 Realm 转发${RESET}"
-    echo -e "${GREEN}3) 国外安装 端口流量狗${RESET}"
-    echo -e "${GREEN}4) 国内安装 端口流量狗${RESET}"
-    echo -e "${GREEN}5) Zelay Realm转发面板${RESET}"
-    echo -e "${GREEN}6) Realm转发(Web面板)${RESET}"
-    echo -e "${GREEN}7) EZRealm转发${RESET}"
+    echo -e "${GREEN}1) realm-xwPF${RESET}"
+    echo -e "${GREEN}2) Zelay Realm转发面板${RESET}"
+    echo -e "${GREEN}3) Realm转发(Web面板)${RESET}"
+    echo -e "${GREEN}4) EZRealm转发${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
@@ -23,32 +20,17 @@ menu() {
             pause
             ;;
         2)
-            echo -e "${GREEN}正在国内环境安装 Realm 转发...${RESET}"
-            wget -qO- https://v6.gh-proxy.org/https://raw.githubusercontent.com/zywe03/realm-xwPF/main/xwPF.sh | sudo bash -s install
-            pause
-            ;;
-        3)
-            echo -e "${GREEN}正在国外环境安装 端口流量狗...${RESET}"
-            wget -O port-traffic-dog.sh https://raw.githubusercontent.com/zywe03/realm-xwPF/main/port-traffic-dog.sh && chmod +x port-traffic-dog.sh && ./port-traffic-dog.sh
-            pause
-            ;;
-        4)
-            echo -e "${GREEN}正在国内环境安装 端口流量狗...${RESET}"
-            wget -O port-traffic-dog.sh https://v6.gh-proxy.org/https://raw.githubusercontent.com/zywe03/realm-xwPF/main/port-traffic-dog.sh && chmod +x port-traffic-dog.sh && ./port-traffic-dog.sh
-            pause
-            ;;
-        5)
             echo -e "${GREEN}正在Zelay Realm转发面板...${RESET}"
             bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/ZelayRealm.sh)
             ;;
-        6)
+        3)
             echo -e "${GREEN}正在Realm转发(Web面板)...${RESET}"
             bash <(curl -fsSL https://raw.githubusercontent.com/hiapb/hia-realm/main/install.sh)
             pause
             ;;
-        7)
+        4)
             echo -e "${GREEN}正在EZRealm转发...${RESET}"
-            bash <(curl -sL https://raw.githubusercontent.com/sistarry/toolbox/main/PROXY/Realm.sh)
+            wget -N https://raw.githubusercontent.com/qqrrooty/EZrealm/main/realm.sh && chmod +x realm.sh && ./realm.sh
             pause
             ;;
         0)
